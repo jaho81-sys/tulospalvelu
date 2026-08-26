0df6e8dc1633330873e9bf6c5f23746fad00c2cd
jaho81-sys <jaho81@gmail.com>
2026-08-26 13:46:56 +0300

    Add API integration helper for WinHk main window

---
+++ TPsource/V52/cbHk/ApiHkIntegration.h
@@ -0,0 +1,57 @@
+// Pekka Pirila's sports timekeeping program (Finnish: tulospalveluohjelma)
+// REST API Integration moduuli pääikkunaan
+// Copyright (C) 2015 Pekka Pirila 
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// KÄYTTO:
+// 1. Lisää tämä tiedosto projektiin: TPsource\V52\cbHk\ApiHkIntegration.cpp
+// 2. Lisää seuraava koodiin WinHk.cpp:n alussa:
+//    #include "ApiHkIntegration.h"
+// 3. Lisää WinHk.h:n julkisiin metodeihin:
+//    void __fastcall ApiSettingsClick(TObject *Sender);
+// 4. Lisää WinHk.cpp:n FormShow-funktioon:
+//    TApiIntegration::GetInstance()->Alusta();
+// 5. Lisää WinHk.cpp:n FormClose-funktioon:
+//    TApiIntegration::GetInstance()->Lopeta();
+
+#ifndef ApiHkIntegrationH
+#define ApiHkIntegrationH
+
+#include <Classes.hpp>
+#include "ApiIntegration.h"
+
+//---------------------------------------------------------------------------
+// Helpperi-funktiot API-integroimiseen pääikkunaan
+namespace ApiHkIntegration
+{
+	// API-ikkunan avaaminen pääikkunasta
+	inline void AvaaMuokkausikkuna(HWND parentHandle)
+	{
+		TApiIntegration::GetInstance()->AvaaMuokkausikkuna(parentHandle);
+	}
+	
+	// API-säikeen käynnistäminen
+	inline void Alusta()
+	{
+		TApiIntegration::GetInstance()->Alusta();
+	}
+	
+	// API-säikeen pysäyttäminen
+	inline void Lopeta()
+	{
+		TApiIntegration::GetInstance()->Lopeta();
+	}
+	
+	// API:n tila
+	inline bool OnAktiivinen()
+	{
+		return TApiIntegration::GetInstance()->OnAktiivinen();
+	}
+}
+
+//---------------------------------------------------------------------------
+#endif
